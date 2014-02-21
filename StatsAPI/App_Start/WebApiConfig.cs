8b39c469458b50fc736d6e4cea15c08d58f46bad
Jonathan MARROT <jonathan.marrot@gmail.com>
2014-02-21 17:48:20 +0100

    Type return XML or JSON
    By default JSON return
    Possible change with "type" parameter

--- StatsAPI/App_Start/WebApiConfig.cs
+++ StatsAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace StatsAPI
@@ -9,13 +11,23 @@ namespace StatsAPI
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            // Set return xml with type parameter
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("type", "xml", new MediaTypeHeaderValue("application/xml")));
+
+            // Set return json with type parameter
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(
+                new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
+
+            // Set return json by default
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
+                name: "Api",
                 routeTemplate: "api/{version}/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
