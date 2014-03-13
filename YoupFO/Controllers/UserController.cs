a440b7fc45498785579dc68df7bd60b59eb10508
Jahdere <paul-louis.barbier@ingesup.com>
2014-03-13 11:32:03 +0100

    UserController and UserModel

---
+++ YoupFO/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YoupFO.Models;
+
+namespace YoupFO.Controllers
+{
+    public class UserController : ApiController
+    {
+        public User[] Get()
+        {
+            return new User[]
+           {
+               new User
+               {
+                   Id = Guid.NewGuid(),
+                   UserName = "Test"
+               },
+               new User
+               {
+                   Id = Guid.NewGuid(),
+                   UserName = "Toto"
+               }
+           };
+        }
+    }
+}
---
+++ YoupFO/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YoupFO.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public String UserName { get; set; }
+
+        public String Email { get; set; }
+
+        public String Password { get; set; }
+
+        public Guid GuidFacebook { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Char Gender { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public String Address { get; set; }
+
+        public int RankId { get; set; }
+
+        public int RoleId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public DateTime DeletedAt { get; set; }
+    }
+}
