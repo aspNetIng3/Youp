c40fc63e0e6663e26540ac9b097f9913cbda7fac
Maxime Dublanc <dubmax.p@gmail.com>
2014-03-20 12:16:10 +0100

    API Forum stats
    olol2

--- StatsAPI/Controllers/ForumController.cs
+++ StatsAPI/Controllers/ForumController.cs
@@ -12,13 +12,45 @@ namespace StatsAPI.Controllers
     {
         YoupEntities db = new YoupEntities();
 
-        // GET api/v1/forum/GetCountMessagesByUser/{user_id}
+        // GET api/v1/forum/GetCountThreadsByTheme/{id}
+        // Retourne le nombre de threads dans la categorie
+        public HttpResponseMessage GetCountThreadsByTheme(int id)
+        {
+            Theme theme = db.Themes.Find(id);
+
+            if (theme == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Categorie introuvable");
+            }
+            else
+            {
+                return Request.CreateResponse(theme.Threads.Count().ToString());
+            }
+        }
+
+        // GET api/v1/forum/GetCountMessagesByThread/{id}
+        // Retourne le nombre de messages dans le thread
+        public HttpResponseMessage GetCountMessagesByThread(int id)
+        {
+            Thread thread = db.Threads.Find(id);
+
+            if (thread == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Thread introuvable");
+            }
+            else
+            {
+                return Request.CreateResponse(thread.Messages.Count().ToString());
+            }
+        }
+
+        // GET api/v1/forum/GetCountMessagesByUser/{userId}
         // Retourne le nombre de messages pour l'utilisateur choisi
         public HttpResponseMessage GetCountMessagesByUser(int userId)
         {
             User user = db.Users.Find(userId);
 
-            if(User == null)
+            if(user == null)
             {
                 return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Utilisateur introuvable");
             }
